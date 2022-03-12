accb1485fd49c73c62063724c0e0456716a01ad4
xssnd <radietsa@gmail.com>
2022-03-12 22:11:44 +0700

    update added code

--- BASIC/002_OperatorNumber.py
+++ BASIC/002_OperatorNumber.py
@@ -11,3 +11,13 @@ print(b / a)    # 0.5
 print(b // a)   # 0
 print(a * c)    # 19.79999
 print(a ** b)   # 216
+
+print(20 % 3)   # 2 
+
+a+=8        # same with a = a + 8
+print(a)    # 14 (6 + 8)
+
+a+=1
+print(a)    #15
+a -=1
+print(a)    #14
