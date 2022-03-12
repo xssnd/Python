8b7457cfcf9b3b5dd92e05180571087c1783962e
xssnd <radietsa@gmail.com>
2022-03-12 22:11:44 +0700

    moving

---
+++ BASIC/000_number/002_OperatorNumber.py
@@ -0,0 +1,23 @@
+a = 6
+b = 3
+c = 3.3
+
+# python with operator 
+d = a + b       
+print(d)        # 9
+print(a + b)    # 9
+print(a + c)    # 9.3
+print(b / a)    # 0.5
+print(b // a)   # 0
+print(a * c)    # 19.79999
+print(a ** b)   # 216
+
+print(20 % 3)   # 2 
+
+a+=8        # same with a = a + 8
+print(a)    # 14 (6 + 8)
+
+a+=1
+print(a)    #15
+a -=1
+print(a)    #14
