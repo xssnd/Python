22014881852f3942eda48fbb3e61479b2e07faa7
xssnd <radietsa@gmail.com>
2022-03-13 06:00:54 +0700

    True and False

---
+++ BASIC/001_Boolean/002_TrueandFalse.py
@@ -0,0 +1,17 @@
+a = 2
+x = a == 2
+print(x)
+if x:
+    print(x, "Is True")
+
+b = a == 3
+print(b)
+if b:
+    print(x, "is True")
+else:
+    print(x, "is False")
+
+# True
+# True Is True
+# False
+# True is False
