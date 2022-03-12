46a7f1aaebfa77cca6bba5550f43af9f6d65854b
xssnd <radietsa@gmail.com>
2022-03-12 22:33:19 +0700

    Python Basic syntax code sample

---
+++ BASIC/002_OperatorNumber.py
@@ -0,0 +1,13 @@
+a = 6
+b = 3
+c = 3.3
+
+# python with operator 
+d = a + b       
+print(d)        # 9
+print(a + b)    # 9
+print(a + c)    # 9.3
+print(b / a)    # 0.5
+print(b // a)   # 0
+print(a * c)    # 19.79999
+print(a ** b)   # 216
