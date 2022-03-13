494c066365ec492b56f73ce85f131cceca1846e3
xssnd <radietsa@gmail.com>
2022-03-13 22:25:10 +0700

    Python Loops List

---
+++ BASIC/002_Loops/002_LoopsLists.py
@@ -0,0 +1,7 @@
+fruits = ["Strawberries", "Pineapple", "Blueberries", "Red grapes"]
+for fruit in fruits:
+    print(fruit)
+
+print("-" * 30)
+for listfruits in ["Mango", "Banana", "Durian", "Rambutan"]:
+    print(listfruits)
